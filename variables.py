81687feef11e7f3605c954ba0a6eab3e10bb3aad
Omar <ogspain@gmail.com>
2019-06-25 01:10:41 -0400

    modularizada la aplicación en funciones, agregado el archivo de variables, uso del modulo nativo de CSV, agregado el archivo requiements para correr en un entorno virtual

---
+++ variables.py
@@ -0,0 +1,7 @@
+'''Las constantes se guardan como variables en el un artivo separado 
+para poder cambiarlas mas facil, y se colocan en mayusculas por que
+son CONSTANTES'''
+
+MY_URL = 'http://www.unomotos.com.ar/'
+
+OUT_FILE = "motos.csv"
